ebabd2609a7bdc9ef3eb6c1debea2a4542174c40
Ma-due <maduee2@naver.com>
2021-11-16 00:50:41 +0900

    feat(*): 프로젝트 시작

---
+++ src/main/java/com/example/oopblackjack/OopBlackjackApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopblackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopBlackjackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopBlackjackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oopblackjack/OopBlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopblackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopBlackjackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
