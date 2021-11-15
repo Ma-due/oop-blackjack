07b56e4e522f95ac144782847f4c6af6b4e34474
Ma-due <maduee2@naver.com>
2021-11-16 01:47:48 +0900

    feat(CardDeck) : 카드 덱

---
+++ src/main/java/com/example/oopblackjack/card/CardDeck.java
@@ -0,0 +1,26 @@
+package com.example.oopblackjack.card;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class CardDeck {
+    private final Stack<Card> cardDeck;
+
+    public CardDeck() {
+        this.cardDeck = generateCard();
+        Collections.shuffle(this.cardDeck);
+    }
+
+    private Stack<Card> generateCard() {
+        Stack<Card> cards = new Stack<>();
+
+        for (Card.Pattern pattern : Card.Pattern.values()) {
+            for (Card.Denomination denomination : Card.Denomination.values()) {
+                Card card = new Card(pattern, denomination);
+                cards.push(card);
+            }
+        }
+
+        return cards;
+    }
+}
