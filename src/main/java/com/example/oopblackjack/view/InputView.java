8ffc26283b2a0ec18d6546f9bb1380e702320b31
Ma-due <maduee2@naver.com>
2021-11-16 21:23:01 +0900

    feat(InputView) : 카드를 더 뽑을지 물어보는 기능

---
+++ src/main/java/com/example/oopblackjack/view/InputView.java
@@ -0,0 +1,17 @@
+package com.example.oopblackjack.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public boolean inputDrawCardOpt() {
+        System.out.println("카드를 더 뽑으시겠습니까? 뽑으려면 1, 아니면 2");
+        String s = scanner.nextLine();
+
+        /* 검증 */
+
+        return Integer.parseInt(s) == 1;
+    }
+}
